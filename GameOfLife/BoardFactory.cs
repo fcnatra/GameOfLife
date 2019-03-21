7be72477806dfeadb1d621086301a820c5853932
frank.torres <frank.torres@ekasa.net>
2019-03-21 08:04:26 +0100

    Refactoring main function

---
+++ GameOfLife/BoardFactory.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class BoardFactory
+    {
+        public enum GameOfLifePatterns
+        {
+            OscilatorBlinker,
+            OscilatorToad,
+            OscilatorBeacon,
+
+            SpaceShipGlider,
+
+            CellularAutomationGosperGliderGun
+        }
+
+        public static List<String> PatterbBoard(GameOfLifePatterns pattern)
+        {
+            // https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+            List<String> resultPattern = null;
+
+            switch (pattern)
+            {
+                case GameOfLifePatterns.OscilatorBlinker:
+                    resultPattern = new List<String>
+                    {
+                        "",
+                        "",
+                        ".***."
+                    };
+                    break;
+                case GameOfLifePatterns.OscilatorToad:
+                    resultPattern = new List<String>
+                    {
+                        "",
+                        "",
+                        "..***.",
+                        ".***.."
+                    };
+                    break;
+                case GameOfLifePatterns.OscilatorBeacon:
+                    resultPattern = new List<String>
+                    {
+                        "",
+                        ".**...",
+                        ".**...",
+                        "...**.",
+                        "...**."
+                    };
+                    break;
+                case GameOfLifePatterns.SpaceShipGlider:
+                    resultPattern = new List<String>
+                    {
+                        "",
+                        "..*.",
+                        "...*.",
+                        ".***.",
+                    };
+                    break;
+                case GameOfLifePatterns.CellularAutomationGosperGliderGun:
+                    // https://en.wikipedia.org/wiki/Gun_(cellular_automaton)#/media/File:Game_of_life_glider_gun.svg
+                    resultPattern = new List<String>
+                    {
+                        "",
+                        ".........................*............",
+                        ".......................*.*............",
+                        ".............**......**............**.",
+                        "............*...*....**............**.",
+                        ".**........*.....*...**...............",
+                        ".**........*...*.**....*.*............",
+                        "...........*.....*.......*............",
+                        "............*...*.....................",
+                        ".............**.......................",
+                    };
+                    break;
+                default:
+                    break;
+            }
+            return resultPattern;
+        }
+    }
+}
---
+++ GameOfLife/GameOfLifeConsoleUI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace GameOfLife
+{
+    public class GameOfLifeConsoleUI : IGameOfLifeUI
+    {
+        public void RunLife(GameOfLifeCoreLogic gameOfLife)
+        {
+            do
+            {
+                SetTitle(gameOfLife.GenerationNumber);
+                DrawBoard(gameOfLife.Board);
+                Thread.Sleep(100);
+                gameOfLife.NextGeneration();
+            } while (!Console.KeyAvailable);
+        }
+
+        private void SetTitle(long generationNumber)
+        {
+            Console.SetCursorPosition(1, 1);
+            Console.Write($"Generation number: {generationNumber}");
+        }
+
+        private void DrawBoard(StringBuilder[] board)
+        {
+            var rows = board.Length;
+            var columns = board[0].Length;
+
+            var rowOffset = 5;
+            var columnOffset = 5;
+
+            for (int row = 0; row < rows; row++)
+                for (int column = 0; column < columns; column++)
+                {
+                    Console.SetCursorPosition(column + columnOffset, row + rowOffset);
+                    Console.Write(board[row][column]);
+                }
+        }
+    }
+}
